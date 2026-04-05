ea110208b59436fe61a0c2868ff90a40626a59dc
Upadhyay <gaganupadhyay.karan@gmail.com>
2026-04-05 16:18:53 +0530

    removed cors only gateway will handle it

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions={
     credentials:true,
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(compression());
 app.use(cookieParser());
