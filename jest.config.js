5d0f2c9df7472f677802f0b2bb0ab64b5998f567
gagan-upadhyay <gaganupadhyay.karan@gmail.com>
2026-04-19 15:05:33 +0530

    added integration tests

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,5 @@ export default {
   testEnvironment: "node",
   transform: {},
   setupFilesAfterEnv: ["./jest.setup.js"],
-  // globalTeardown: "./jest.teardown.js",
-  // moduleNameMapper: {
-  //   "^(\\.{1,2}/.*)\\.js$": "$1"  // Fix ESM imports
-  // }
+  testTimeout:30000,
 };
